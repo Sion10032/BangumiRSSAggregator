3e811bb34600a4a3c950954be15ebf6998e5e8b9
Sion10032 <sion10032@hotmail.com>
2024-08-12 21:37:37 +0800

    1. add fetch button for Feeds component; 2. set client's timeout to 20000ms.

--- BangumiRSSAggregator.Client/src/components/feeds/Feeds.tsx
+++ BangumiRSSAggregator.Client/src/components/feeds/Feeds.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { Card, message } from 'antd';
-import { MdAdd, MdDelete, MdEdit } from "react-icons/md";
+import { MdAdd, MdDelete, MdEdit, MdRefresh } from "react-icons/md";
 
 import type { TableColumnsType } from 'antd';
 import type { FeedSource } from '@/shared/types/models';
@@ -8,6 +8,7 @@ import type { FeedSource } from '@/shared/types/models';
 import AutoHeightTable from '../AutoHeightTable'
 import AddFeedFormModal from './EditFeed';
 import { getRestClient } from '@/shared/rest-client';
+import client from '@/shared/client';
 
 function Feeds() {
   const [ messageApi, contextHolder ] = message.useMessage();
@@ -37,6 +38,22 @@ function Feeds() {
     }
     setRefreshData(!refreshData);
   };
+  const fetchFeed = async () => {
+    if (selectedRowKeys.current.length <= 0) {
+      return;
+    }
+
+    const resp = await client
+      .get(`feeds/${selectedRowKeys.current[0]}/fetch`)
+      .json<boolean>();
+    if (resp) {
+      messageApi.success(`Fetching for feed-${selectedRowKeys.current[0]} success.`);
+    }
+    else {
+      messageApi.warning(`Fetching for feed-${selectedRowKeys.current[0]} failed.`);
+    }
+  };
+
   const onAddConfirm = async (value : FeedSource) => {
     if (await restClient.add(value)) {
       messageApi.success("add feed success.");
@@ -73,6 +90,7 @@ function Feeds() {
           <MdAdd onClick={openEditModal}/>,
           <MdEdit />,
           <MdDelete onClick={deleteFeed}/>,
+          <MdRefresh onClick={fetchFeed}/>,
         ]}>
         <AutoHeightTable
           offset={8}
--- BangumiRSSAggregator.Client/src/shared/client.ts
+++ BangumiRSSAggregator.Client/src/shared/client.ts
@@ -1,7 +1,8 @@
 import ky, { Options } from "ky";
 
 const option : Options = {
-    prefixUrl: "http://localhost:5072/api"
+  prefixUrl: "http://localhost:5072/api",
+  timeout: 20000,
 };
 
 const client = ky.extend(option);
